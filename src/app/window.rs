ac7480a533ddee4386a5074b659613ce4b9d06b5
nabbisen <nabbisen@scqr.net>
2026-01-21 08:05:14 +0900

    clipboard copy; ui message; error handling on invalid json str

--- src/app/window.rs
+++ src/app/window.rs
@@ -1,7 +1,8 @@
+use arboard::Clipboard;
 use iced::{
     Alignment, Element,
     Length::Fill,
-    widget::{button, column, container, row, text_editor, text_input},
+    widget::{Text, button, column, container, row, text_editor, text_input},
 };
 use serde_json::Value;
 
@@ -11,12 +12,15 @@ use crate::app::util::{decode, encode};
 pub struct Window {
     encoded_value: String,
     decoded_value: text_editor::Content,
+    ui_message: Option<String>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     EncodedChanged(String),
+    CopyEncoded,
     DecodedChanged(text_editor::Action),
+    CopyDecoded,
     Decode,
     Encode,
     Clear,
@@ -27,14 +31,18 @@ impl Window {
         Self {
             encoded_value: String::new(),
             decoded_value: text_editor::Content::new(),
+            ui_message: None,
         }
     }
 
     pub fn view(&self) -> Element<'_, Message> {
-        let encoded = text_input("JWT here...", &self.encoded_value)
-            .on_input(Message::EncodedChanged)
-            .padding(10)
-            .size(20);
+        let encoded = row![
+            text_input("JWT here...", &self.encoded_value)
+                .on_input(Message::EncodedChanged)
+                .padding(10)
+                .size(20),
+            button("Copy").on_press(Message::CopyEncoded)
+        ];
 
         let buttons = row![
             button("Decode").on_press(Message::Decode).padding(10),
@@ -43,17 +51,24 @@ impl Window {
         ]
         .spacing(20);
 
-        let decoded = text_editor(&self.decoded_value)
-            .placeholder("JSON here...")
-            .on_action(Message::DecodedChanged)
-            .height(Fill)
-            .padding(10)
-            .size(20);
+        let decoded = row![
+            text_editor(&self.decoded_value)
+                .placeholder("JSON here...")
+                .on_action(Message::DecodedChanged)
+                .height(Fill)
+                .padding(10)
+                .size(20),
+            button("Copy").on_press(Message::CopyDecoded)
+        ];
 
-        let content = column![encoded, buttons, decoded]
-            .spacing(15)
-            .padding(20)
-            .align_x(Alignment::Start);
+        let mut content = column![];
+        if let Some(ui_message) = self.ui_message.clone() {
+            content = content.push(Text::new(ui_message));
+        }
+        for child in [encoded, buttons, decoded] {
+            content = content.push(child);
+        }
+        content = content.spacing(15).padding(20).align_x(Alignment::Start);
 
         container(content)
             .width(Fill)
@@ -66,7 +81,27 @@ impl Window {
     pub fn update(&mut self, message: Message) {
         match message {
             Message::EncodedChanged(s) => self.encoded_value = s,
+            Message::CopyEncoded => {
+                if !self.encoded_value.is_empty()
+                    && let Ok(mut clipboard) = Clipboard::new()
+                {
+                    // todo: error handling
+                    let _ = clipboard.set_text(self.encoded_value.clone());
+
+                    self.ui_message = Some("Copied".to_owned());
+                }
+            }
             Message::DecodedChanged(action) => self.decoded_value.perform(action),
+            Message::CopyDecoded => {
+                if !self.decoded_value.is_empty()
+                    && let Ok(mut clipboard) = Clipboard::new()
+                {
+                    // todo: error handling
+                    let _ = clipboard.set_text(self.decoded_value.text());
+
+                    self.ui_message = Some("Copied".to_owned());
+                }
+            }
             Message::Decode => {
                 if self.encoded_value.is_empty() {
                     return;
@@ -86,8 +121,14 @@ impl Window {
                 }
 
                 let s = self.decoded_value.text();
-                // todo: exit unexpectedly when JSON value is abc instead of "abc" - show error message
-                let v: Value = json5::from_str(&s).expect("failed to get json from str");
+                let v: Value = match json5::from_str(&s) {
+                    Ok(x) => x,
+                    Err(err) => {
+                        self.ui_message =
+                            Some(format!("failed to get json from str: {}", err.to_string()));
+                        return;
+                    }
+                };
                 let p = serde_json::to_string_pretty(&v).expect("failed to prettify");
                 if s != p {
                     self.decoded_value = text_editor::Content::with_text(p.as_str());
@@ -100,6 +141,7 @@ impl Window {
             Message::Clear => {
                 self.encoded_value = String::new();
                 self.decoded_value = text_editor::Content::new();
+                self.ui_message = None;
             }
         }
     }
