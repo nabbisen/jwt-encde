bb6f0dab15e714fcece5f77787444923d7de12c7
nabbisen <nabbisen@scqr.net>
2026-01-21 14:41:54 +0900

    json header is now supported

--- src/app/jwt.rs
+++ src/app/jwt.rs
@@ -1,14 +1,24 @@
 use base64::{Engine, prelude::BASE64_URL_SAFE_NO_PAD};
-use jsonwebtoken::{self, EncodingKey, Header};
+use jsonwebtoken::{self, EncodingKey, Header, decode_header};
 use serde_json::Value;
 
 /// encode (鍵なし / alg: none)
-pub fn encode(payload: &Value) -> Result<String, String> {
+pub fn encode(header: Option<&Header>, payload: Option<&Value>) -> Result<String, String> {
     // todo: header
-    let header = Header::default();
+    let header = if let Some(header) = header {
+        header
+    } else {
+        &Header::default()
+    };
+
+    let payload = if let Some(payload) = payload {
+        payload
+    } else {
+        &Value::Null
+    };
 
     let token = jsonwebtoken::encode(
-        &header,
+        header,
         payload,
         &EncodingKey::from_secret(&[]), // ダミーのキー
     )
@@ -18,26 +28,36 @@ pub fn encode(payload: &Value) -> Result<String, String> {
 }
 
 /// decode (署名検証なし)
-pub fn decode(s: &str) -> Result<Value, String> {
-    // --- 1. ヘッダーのデコード (jsonwebtoken の機能を使用) ---
-    // let header = decode_header(s.as_ref()).expect("failed to decode");
-    // println!("--- Header (v9 decode_header) ---");
-    // println!("{:#?}\n", header);
+pub fn decode(s: &str) -> Result<(Option<Header>, Option<Value>), String> {
+    let header = match decode_header(s) {
+        Ok(x) => Some(x),
+        Err(err) => return Err(format!("failed to decode: {}", err.to_string())),
+    };
 
-    // --- 2. ペイロードのデコード (手動 Base64 デコード) ---
-    // JWTは "Header.Payload.Signature" の形式なので、2番目の要素を取得する
+    // JWT は "Header.Payload.Signature" の形式。2 番目の要素を取得
     let parts: Vec<&str> = s.split('.').collect();
     if parts.len() < 2 {
-        return Err("Invalid token format".to_owned());
+        return Err("failed to decode payload: invalid token format".to_owned());
     }
-
     let payload_b64 = parts[1];
-    let payload_bytes = BASE64_URL_SAFE_NO_PAD
-        .decode(payload_b64)
-        .expect("failed to decode base64 payload");
+    let payload_bytes = match BASE64_URL_SAFE_NO_PAD.decode(payload_b64) {
+        Ok(x) => x,
+        Err(err) => {
+            return Err(format!(
+                "failed to decode base64 payload: {}",
+                err.to_string()
+            ));
+        }
+    };
+    let payload = match serde_json::from_slice(&payload_bytes) {
+        Ok(x) => Some(x),
+        Err(err) => {
+            return Err(format!(
+                "failed to decode serialization: {}",
+                err.to_string()
+            ));
+        }
+    };
 
-    let payload: Value =
-        serde_json::from_slice(&payload_bytes).expect("failed to decode serialization");
-
-    Ok(payload)
+    Ok((header, payload))
 }
--- src/app/window.rs
+++ src/app/window.rs
@@ -1,18 +1,22 @@
 use arboard::Clipboard;
 use iced::{
     Alignment, Element,
-    Length::Fill,
-    widget::{Text, button, column, container, row, text_editor, text_input},
+    Length::{Fill, FillPortion},
+    alignment::Horizontal::Right,
+    widget::{Text, button, column, container, row, text, text_editor, text_input},
 };
+use jsonwebtoken::Header;
 use serde_json::Value;
 
 use crate::app::jwt::{decode, encode};
 
 #[derive(Default)]
 pub struct Window {
-    encoded_str: String,
-    decoded_str: text_editor::Content,
-    payload: Value,
+    jwt_str: String,
+    jwt_header_json_str: text_editor::Content,
+    jwt_header: Option<Header>,
+    jwt_payload_json_str: text_editor::Content,
+    jwt_payload: Option<Value>,
     ui_message: Option<String>,
 }
 
@@ -20,8 +24,10 @@ pub struct Window {
 pub enum Message {
     EncodedChanged(String),
     CopyEncoded,
-    DecodedChanged(text_editor::Action),
-    CopyDecoded,
+    JwtHeaderChanged(text_editor::Action),
+    JwtPayloadChanged(text_editor::Action),
+    CopyJwtHeaderJsonStr,
+    CopyJwtPayloadJsonStr,
     Decode,
     Encode,
     Clear,
@@ -29,13 +35,13 @@ pub enum Message {
 
 impl Window {
     pub fn view(&self) -> Element<'_, Message> {
-        let encoded = row![
-            text_input("JWT here...", &self.encoded_str)
+        let encoded = row![column![
+            row![text("JWT"), button("Copy").on_press(Message::CopyEncoded)].spacing(20),
+            text_input("JWT here...", &self.jwt_str)
                 .on_input(Message::EncodedChanged)
                 .padding(10)
                 .size(20),
-            button("Copy").on_press(Message::CopyEncoded)
-        ];
+        ]];
 
         let buttons = row![
             button("Decode").on_press(Message::Decode).padding(10),
@@ -45,20 +51,47 @@ impl Window {
         .spacing(20);
 
         let decoded = row![
-            text_editor(&self.decoded_str)
-                .placeholder("JSON here...")
-                .on_action(Message::DecodedChanged)
-                .height(Fill)
-                .padding(10)
-                .size(20),
-            button("Copy").on_press(Message::CopyDecoded)
+            column![
+                row![
+                    text("Header"),
+                    button("Copy").on_press(Message::CopyJwtHeaderJsonStr)
+                ]
+                .spacing(20),
+                text_editor(&self.jwt_header_json_str)
+                    .placeholder("JSON str of JWT header here...")
+                    .on_action(Message::JwtHeaderChanged)
+                    .height(Fill)
+                    .padding(10)
+                    .size(20),
+            ]
+            .width(FillPortion(4))
+            .height(Fill),
+            column![
+                row![
+                    text("Payload"),
+                    button("Copy").on_press(Message::CopyJwtPayloadJsonStr)
+                ]
+                .spacing(20),
+                text_editor(&self.jwt_payload_json_str)
+                    .placeholder("JSON str of JWT payload here...")
+                    .on_action(Message::JwtPayloadChanged)
+                    .height(Fill)
+                    .padding(10)
+                    .size(20),
+            ]
+            .width(FillPortion(6))
+            .height(Fill),
         ];
+
+        let footer = row![text(
+            "jwt-encde is GUI JWT encoder / decoder - Local, private, easy. Thank you for using. Dev as OSS @ https://github.com/nabbisen/jwt-encde"
+        ).width(Fill).align_x(Right).color(iced::color!(0x878787))];
 
         let mut content = column![];
         if let Some(ui_message) = self.ui_message.clone() {
             content = content.push(Text::new(ui_message));
         }
-        for child in [encoded, buttons, decoded] {
+        for child in [encoded, buttons, decoded, footer] {
             content = content.push(child);
         }
         content = content.spacing(15).padding(20).align_x(Alignment::Start);
@@ -73,71 +106,107 @@ impl Window {
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::EncodedChanged(s) => self.encoded_str = s,
+            Message::EncodedChanged(s) => self.jwt_str = s,
             Message::CopyEncoded => {
-                if !self.encoded_str.is_empty()
+                if !self.jwt_str.is_empty()
                     && let Ok(mut clipboard) = Clipboard::new()
                 {
                     // todo: error handling
-                    let _ = clipboard.set_text(self.encoded_str.clone());
+                    let _ = clipboard.set_text(self.jwt_str.clone());
 
                     self.ui_message = Some("Copied".to_owned());
                 }
             }
-            Message::DecodedChanged(action) => self.decoded_str.perform(action),
-            Message::CopyDecoded => {
-                if !self.decoded_str.is_empty()
+            Message::JwtHeaderChanged(action) => self.jwt_header_json_str.perform(action),
+            Message::JwtPayloadChanged(action) => self.jwt_payload_json_str.perform(action),
+            Message::CopyJwtHeaderJsonStr => {
+                if !self.jwt_header_json_str.is_empty()
                     && let Ok(mut clipboard) = Clipboard::new()
                 {
                     // todo: error handling
-                    let _ = clipboard.set_text(self.decoded_str.text());
+                    let _ = clipboard.set_text(self.jwt_header_json_str.text());
+
+                    self.ui_message = Some("Copied".to_owned());
+                }
+            }
+            Message::CopyJwtPayloadJsonStr => {
+                if !self.jwt_payload_json_str.is_empty()
+                    && let Ok(mut clipboard) = Clipboard::new()
+                {
+                    // todo: error handling
+                    let _ = clipboard.set_text(self.jwt_payload_json_str.text());
 
                     self.ui_message = Some("Copied".to_owned());
                 }
             }
             Message::Decode => {
                 // todo: button disabled
-                if self.encoded_str.is_empty() {
+                if self.jwt_str.is_empty() {
                     return;
                 }
 
-                match decode(self.encoded_str.as_str()) {
-                    Ok(x) => {
-                        self.payload = x;
-                        let s = serde_json::to_string_pretty(&self.payload)
-                            .expect("failed to get str from json value");
-                        self.decoded_str = text_editor::Content::with_text(s.as_str());
+                match decode(self.jwt_str.as_str()) {
+                    Ok((header, payload)) => {
+                        self.jwt_header = header;
+                        if let Some(header) = self.jwt_header.as_ref() {
+                            let s = serde_json::to_string_pretty(header)
+                                .expect("failed to get str from json value");
+                            self.jwt_header_json_str = text_editor::Content::with_text(s.as_str());
+                        }
+                        self.jwt_payload = payload;
+                        if let Some(payload) = self.jwt_payload.as_ref() {
+                            let s = serde_json::to_string_pretty(payload)
+                                .expect("failed to get str from json value");
+                            self.jwt_payload_json_str = text_editor::Content::with_text(s.as_str());
+                        }
                     }
-                    Err(_) => self.decoded_str = text_editor::Content::new(),
+                    Err(_) => self.clear_decoded(),
                 }
             }
             Message::Encode => {
-                // todo: button disabled
-                if self.decoded_str.text().is_empty() {
-                    return;
-                }
-
-                let s = self.decoded_str.text();
-                let v: Value = match json5::from_str(&s) {
-                    Ok(x) => x,
+                match json5::from_str(self.jwt_header_json_str.text().as_str()) {
+                    Ok(x) => self.jwt_header = x,
                     Err(err) => {
-                        self.ui_message =
-                            Some(format!("failed to get json from str: {}", err.to_string()));
+                        self.ui_message = Some(format!(
+                            "failed to convert header to json: {}",
+                            err.to_string()
+                        ));
                         return;
                     }
                 };
-                self.payload = v;
-                self.encoded_str = match encode(&self.payload) {
-                    Ok(x) => x,
-                    Err(_) => String::default(),
+                match json5::from_str(self.jwt_payload_json_str.text().as_str()) {
+                    Ok(x) => self.jwt_payload = x,
+                    Err(err) => {
+                        self.ui_message = Some(format!(
+                            "failed to convert payload to json: {}",
+                            err.to_string()
+                        ));
+                        return;
+                    }
+                };
+                match encode(self.jwt_header.as_ref(), self.jwt_payload.as_ref()) {
+                    Ok(x) => self.jwt_str = x,
+                    Err(_) => self.clear_encoded(),
                 };
             }
-            Message::Clear => {
-                self.encoded_str = String::default();
-                self.payload = Value::default();
-                self.decoded_str = text_editor::Content::default();
-                self.ui_message = None;
-            }
+            Message::Clear => self.clear(),
         }
+    }
+
+    fn clear(&mut self) {
+        self.clear_encoded();
+        self.clear_decoded();
+        self.ui_message = None;
+    }
+
+    fn clear_encoded(&mut self) {
+        self.jwt_str = String::default();
+    }
+
+    fn clear_decoded(&mut self) {
+        self.jwt_header = None;
+        self.jwt_header_json_str = text_editor::Content::default();
+        self.jwt_payload = None;
+        self.jwt_payload_json_str = text_editor::Content::default();
     }
 }
